ec66d6b74c20990e66cf0fede165a93b98b4faa8
Jukka Ollila <jukka.ollila@iki.fi>
2013-02-18 21:21:28 +0200

    popup: redo popup menu handling
     * provide handy wrappers for adding items into the popup
    
     * provide handy wrappers for (item, popup) pairs, for memory
       management
    
     * hook "button-press-event" instead of "activate" for menu
       items. Seems to wok

--- popup.c
+++ popup.c
@@ -1,14 +1,111 @@
 
 #include "popup.h"
 
+#include <stdlib.h>
+
 #include <gtk/gtk.h>
 
-static void setup_menu_for_track(GtkMenuShell *menu,
-				 sp_track *track,
-				 popup_selection_cb popup_selection_cb,
-				 void *user_data)
+struct popup_item {
+	struct popup_item *next;
+	struct item *item;
+	struct popup *popup;
+};
+
+struct popup {
+	GtkMenu *menu;
+
+	popup_selection_cb selection_cb;
+	void *cb_data;
+
+	struct popup_item *item_head, *item_tail;
+
+};
+
+
+static void popup_item_activate(GtkWidget *widget, GdkEventButton *event, struct popup_item *pitem)
 {
-	gtk_menu_shell_append(menu, gtk_menu_item_new_with_label(sp_track_name(track)));
+	pitem->popup->selection_cb(pitem->item, pitem->popup->cb_data);
+}
+
+static void add_item(struct popup *popup, struct item *item, const char *name)
+{
+	struct popup_item *pitem;
+	GtkWidget *menu_item;
+
+	pitem = malloc(sizeof(*pitem));
+	if (pitem == NULL) {
+		fprintf(stderr, "%s() out of memory\n", __func__);
+	}
+	pitem->popup = popup;
+	pitem->item = item;
+	pitem->next = NULL;
+
+	if (popup->item_tail != NULL) {
+		popup->item_tail->next = pitem;
+	} else {
+		popup->item_head = popup->item_tail = pitem;
+	}
+
+	menu_item = gtk_menu_item_new_with_label(name);
+	/* hrm. One would assume "activate" signal works.
+	 * We're doing something wrong, as it doesn't.
+	 *
+	 * No worries, "button-press-event" to the rescue
+	 */
+	g_signal_connect(menu_item, "button-press-event",
+			 G_CALLBACK(popup_item_activate), pitem);
+
+
+
+	gtk_menu_shell_append(GTK_MENU_SHELL(popup->menu), menu_item);
+
+}
+
+static void add_item_artist(struct popup *popup, sp_artist *artist)
+{
+	add_item(popup, item_init_artist(artist), sp_artist_name(artist));
+}
+
+static void add_item_album(struct popup *popup, sp_album *album)
+{
+	add_item(popup, item_init_album(album), sp_album_name(album));
+}
+
+static void setup_menu_for_item(struct popup *popup,
+				struct item *item)
+{
+	switch (item_type(item)) {
+	case ITEM_NONE:
+	case ITEM_PLAYLIST:
+	case ITEM_SEARCH:
+	case ITEM_ARTIST:
+	case ITEM_ALBUM:
+	case ITEM_ALBUMBROWSE:
+		gtk_menu_shell_append(GTK_MENU_SHELL(popup->menu),
+				      gtk_menu_item_new_with_label("I am out of options."
+								   " Why are we here?"));
+		break;
+	case ITEM_TRACK:
+		add_item_artist(popup,sp_track_artist(item_track(item), 0));
+		add_item_album(popup, sp_track_album(item_track(item)));
+		break;
+	}
+}
+
+void popup_destroy(GtkMenuShell *menu, struct popup *popup)
+{
+	struct popup_item *pitem;
+
+	gtk_widget_destroy(GTK_WIDGET(popup->menu));
+
+	for (pitem = popup->item_head; pitem != NULL;) {
+		struct popup_item *saved = pitem->next;
+		item_free(pitem->item);
+		free(pitem);
+		pitem = saved;
+	}
+
+	free(popup);
 }
 
 
@@ -17,34 +114,28 @@ void popup_show(struct item *item, const char *name,
 		popup_selection_cb popup_selection_cb,
 		void *user_data)
 {
-	GtkMenu *menu;
+	struct popup *popup;
 
-	menu = GTK_MENU(gtk_menu_new());
-	g_signal_connect(menu, "deactivate", G_CALLBACK(gtk_widget_destroy), NULL);
-
-	gtk_menu_set_title(menu, name);
-
-	switch (item_type(item)) {
-	case ITEM_NONE:
-	case ITEM_PLAYLIST:
-	case ITEM_SEARCH:
-	case ITEM_ARTIST:
-	case ITEM_ALBUM:
-	case ITEM_ALBUMBROWSE:
-		gtk_menu_shell_append(GTK_MENU_SHELL(menu),
-				      gtk_menu_item_new_with_label("I am out of options."
-								   " Why are we here?"));
-		break;
-	case ITEM_TRACK:
-		setup_menu_for_track(GTK_MENU_SHELL(menu), item_track(item),
-				     popup_selection_cb, user_data);
-		break;
+	popup = malloc(sizeof(*popup));
+	if (popup == NULL) {
+		fprintf(stderr, "%s(): Out of memory\n", __func__);
+		return;
 	}
+	popup->selection_cb = popup_selection_cb;
+	popup->cb_data = user_data;
+	popup->item_head = popup->item_tail = NULL;
 
+	popup->menu = GTK_MENU(gtk_menu_new());
+	g_signal_connect(popup->menu, "deactivate",
+			 G_CALLBACK(popup_destroy), popup);
 
-	gtk_widget_show_all(GTK_WIDGET(menu));
+	gtk_menu_set_title(popup->menu, name);
 
-	gtk_menu_popup(menu,
+	setup_menu_for_item(popup, item);
+
+	gtk_widget_show_all(GTK_WIDGET(popup->menu));
+
+	gtk_menu_popup(popup->menu,
 		       (GtkWidget *)NULL,
 		       (GtkWidget *)NULL,
 		       (GtkMenuPositionFunc)NULL,
