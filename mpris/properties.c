5de9ab09ccd61e3f5e555ca9a0982f2bca626672
Jukka Ollila <jukka.ollila@iki.fi>
2013-11-24 03:47:11 +0200

    mpris: handle org.freedesktop.mpris.MediaPlayer2 property accessors
    None of them are exactly dynamic, so support is kind of trivial.

--- mpris/properties.c
+++ mpris/properties.c
@@ -5,6 +5,92 @@
 #include <stdio.h>
 #include <string.h>
 
+static void property_get_string(DBusConnection *dbus, DBusMessage *msg,
+				const char *s)
+{
+	DBusMessage *reply;
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_message_append_args(reply,
+				 DBUS_TYPE_STRING, &s,
+				 DBUS_TYPE_INVALID);
+	dbus_connection_send(dbus, reply, (dbus_uint32_t *)NULL);
+	dbus_message_unref(reply);
+}
+
+static void property_get_identity(DBusConnection *dbus, DBusMessage *msg,
+				  const struct remote_callback_ops *cb_ops,
+				  const void *cb_data)
+{
+	property_get_string(dbus, msg, "spthui");
+}
+
+
+static void property_get_desktopentry(DBusConnection *dbus, DBusMessage *msg,
+				      const struct remote_callback_ops *cb_ops,
+				      const void *cb_data)
+{
+	property_get_string(dbus, msg, "Spthui");
+}
+
+static void property_get_boolean_false(DBusConnection *dbus, DBusMessage *msg,
+				       const struct remote_callback_ops *cb_ops,
+				       const void *cb_data)
+{
+	DBusMessage *reply;
+	int val = 0;
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_message_append_args(reply,
+				 DBUS_TYPE_BOOLEAN, &val,
+				 DBUS_TYPE_INVALID);
+	dbus_connection_send(dbus, reply, (dbus_uint32_t *)NULL);
+	dbus_message_unref(reply);
+}
+
+static void property_get_supportedurischemes(DBusConnection *dbus, DBusMessage *msg,
+					     const struct remote_callback_ops *cb_ops,
+					     const void *cb_data)
+{
+	DBusMessage *reply;
+	char *schemes[] = { "spthui" };
+	char **schemesp = schemes;
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_message_append_args(reply,
+				 DBUS_TYPE_ARRAY, DBUS_TYPE_STRING, &schemesp, 1,
+				 DBUS_TYPE_INVALID);
+	dbus_connection_send(dbus, reply, (dbus_uint32_t *)NULL);
+	dbus_message_unref(reply);
+}
+
+static void property_set_noop(DBusConnection *dbus, DBusMessage *msg,
+			      const struct remote_callback_ops *cb_ops,
+			      const void *cb_data)
+{
+	DBusMessage *reply;
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_connection_send(dbus, reply, (dbus_uint32_t *)NULL);
+	dbus_message_unref(reply);
+}
+
+static void property_get_empty_string_array(DBusConnection *dbus, DBusMessage *msg,
+					    const struct remote_callback_ops *cb_ops,
+					    const void *cb_data)
+{
+	DBusMessage *reply;
+	char *empty = "";
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_message_append_args(reply,
+				 DBUS_TYPE_ARRAY, DBUS_TYPE_STRING, &empty, 0,
+				 DBUS_TYPE_INVALID);
+	dbus_connection_send(dbus, reply, (dbus_uint32_t *)NULL);
+	dbus_message_unref(reply);
+}
+
+
 static const struct property_handler {
 
 	const char *iface;
@@ -17,6 +103,61 @@ static const struct property_handler {
 		    const struct remote_callback_ops *cb_ops, const void *cb_data);
 
 } property_handlers[] = {
+
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "SupportedMimeTypes",
+		.get = property_get_empty_string_array,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "SupportedUriSchemes",
+		.get = property_get_supportedurischemes,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "CanQuit",
+		.get = property_get_boolean_false,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "CanRaise",
+		.get = property_get_boolean_false,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "CanSetFullScreen",
+		.get = property_get_boolean_false,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "FullScreen",
+		.get = property_get_boolean_false,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "HasTrackList",
+		.get = property_get_boolean_false,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "DesktopEntry",
+		.get = property_get_desktopentry,
+		.set = property_set_noop,
+	},
+	{
+		.iface = "org.freedesktop.mpris.MediaPlayer2",
+		.property = "Identity",
+		.get = property_get_identity,
+		.set = property_set_noop,
+	},
 
 	{ .iface = NULL, .property = NULL, .get = NULL, .set = NULL },
 
