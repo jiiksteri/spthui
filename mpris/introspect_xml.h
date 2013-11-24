3569813bdf01f2c08eef3cfe2c452e7bd649a618
Jukka Ollila <jukka.ollila@iki.fi>
2013-11-24 06:14:57 +0200

    mpris: add introspection xml macros for *.MediaPlayer2.Player types

--- mpris/introspect_xml.h
+++ mpris/introspect_xml.h
@@ -17,6 +17,13 @@
 #define XML_PROP_BOOLEAN_RW(n)     "<property access=\"readwrite\" type=\"b\" name=\"" #n "\" />"
 #define XML_PROP_STRING_RO(n)      "<property access=\"readwrite\" type=\"s\" name=\"" #n "\" />"
 #define XML_PROP_STRINGARRAY_RO(n) "<property access=\"read\" type=\"as\" name=\"" #n "\" />"
+#define XML_PROP_DOUBLE_RO(n)      "<property access=\"read\" type=\"d\" name=\"" #n "\" />"
+#define XML_PROP_DOUBLE_RW(n)      "<property access=\"readwrite\" type=\"d\" name=\"" #n "\" />"
+#define XML_PROP_INT64_RO(n)       "<property access=\"read\" type=\"x\" name=\"" #n "\" />"
+
+
+/* See note about dict assumptions below */
+#define XML_PROP_DICT_RO(n)        "<property access=\"read\" type=\"a{sv}\" name=\"" #n "\" />"
 
 #define XML_VOID_METHOD(n) "<method name=\"" #n "\" />"
 
@@ -27,9 +34,18 @@
 #define XML_METHOD_ARG_STRING_OUT(n)  "<arg direction=\"out\" name=\"" #n "\" type=\"s\" />"
 #define XML_METHOD_ARG_VARIANT_IN(n)  "<arg direction=\"in\" name=\"" #n "\" type=\"v\" />"
 #define XML_METHOD_ARG_VARIANT_OUT(n) "<arg direction=\"out\" name=\"" #n "\" type=\"v\" />"
+#define XML_METHOD_ARG_INT64_IN(n)    "<arg direction=\"in\" name=\"" #n "\" type=\"x\" />"
+#define XML_METHOD_ARG_OBJECT_IN(n)    "<arg direction=\"in\" name=\"" #n "\" type=\"o\" />"
 
 /* Weeell we assume it's an array of (string,variant) tuples and we
  * have no other cases currently
  */
 #define XML_METHOD_ARG_DICT_OUT(n)    "<arg direction=\"out\" name=\"" #n "\" type=\"a{sv}\" />"
+
+
+#define XML_SIGNAL_START(n) "<signal name=\"" #n "\">"
+#define XML_SIGNAL_END(n)   "</signal>"
+
+#define XML_SIGNAL_ARG_INT64(n) "<arg name=\"" #n "\" type=\"x\" />"
+
 #endif
